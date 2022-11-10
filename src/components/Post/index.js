33290b44659f37c72af6afcbed2f696db7193247
ionarose <ionaroseuk@gmail.com>
2022-11-10 16:21:08 +0000

    task 5

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import './index.css';
 
-function Post() {
-  return <article></article>;
+function Post({text, author, src, title, date, highlights, alt}) {
+  return <article>
+<h3>{title}</h3>
+<img src={src} alt={alt} />
+<p>{author}</p>
+<p>{text}</p>
+<p>{date}</p>
+<p>{highlights}</p>
+  </article>;
 }
 
 export default Post;
